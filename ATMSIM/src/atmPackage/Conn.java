08f45b8b1b0bafc24a26773d5eba8abf2825f834
Syed Nabeel <102635993+Syed-Nabeel02@users.noreply.github.com>
2022-05-21 08:10:09 -0400

    Update Conn.java

--- ATMSIM/src/atmPackage/Conn.java
+++ ATMSIM/src/atmPackage/Conn.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class Conn {
 	public Connection c;
 	public Statement s;
-	static String databaseName = "mydb";
+	static String databaseName = "my_db";
 	static String url = "jdbc:mysql://localhost:3306/" + databaseName;
 	
 	static String username = "root";
